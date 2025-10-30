32577aae4dc34694b9c2f733228d5ceb7adaa4ee
Barrenboat <lyz012@qq.com>
2025-10-30 09:54:45 +0800

    feat: 为Integration section实现了滚动动画效果

--- src/sections/Integrations.tsx
+++ src/sections/Integrations.tsx
@@ -64,6 +64,7 @@ export default function Integrations() {
                         <IntegrationColumns
                             integrations={integrations.slice().reverse()}
                             className="hidden md:flex"
+                            reverse={true}
                         />
                     </div>
                 </div>
