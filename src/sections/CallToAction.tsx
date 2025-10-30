3939a7b9e22b985c676d38ea7ace33ebe6708aa7
Barrenboat <lyz012@qq.com>
2025-10-30 11:06:28 +0800

    feat: 为CTA section添加了横向滚动动画

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,17 +1,30 @@
+"use client";
+
+import { motion } from "framer-motion";
+
 export default function CallToAction() {
     return (
         <section className="py-24">
             <div className="overflow-x-clip py-4 px-2 flex">
-                <div className="flex flex-none gap-16 text-7xl md:text-8xl font-medium">
+                <motion.div
+                    animate={{ x: "-50%" }}
+                    transition={{
+                        duration: 30,
+                        ease: "linear",
+                        repeat: Infinity,
+                    }}
+                    className="flex flex-none gap-16 pr-16 text-7xl md:text-8xl font-medium"
+                >
                     {Array.from({ length: 10 }, (_, index) => (
                         <div key={index} className="flex items-center gap-14">
-                            <span className="text-lime-400 text-7xl">&#10038;</span>
+                            <span className="text-lime-400 text-7xl">
+                                &#10038;
+                            </span>
                             <span>Try it for free</span>
                         </div>
                     ))}
-                </div>
+                </motion.div>
             </div>
         </section>
     );
 }
- 
