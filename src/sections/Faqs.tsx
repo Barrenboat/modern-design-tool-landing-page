499b9b3abe035120d47bb9e4dfaf1835084f61fd
Barrenboat <lyz012@qq.com>
2025-10-30 10:56:03 +0800

    feat: 实现了faq卡片展开收回的动画效果

--- src/sections/Faqs.tsx
+++ src/sections/Faqs.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import Tag from "@/components/Tag";
+import { useState } from "react";
 import { twMerge } from "tailwind-merge";
+import { motion, AnimatePresence } from "framer-motion";
 
 const faqs = [
     {
@@ -25,7 +29,8 @@ const faqs = [
 ];
 
 export default function Faqs() {
-    const selectedIndex = 0;
+    const [selectedIndex, setSelectedIndex] = useState(0);
+
     return (
         <section className="py-24">
             <div className="container">
@@ -36,44 +41,77 @@ export default function Faqs() {
                     Questions? We&apos;ve got{" "}
                     <span className="text-lime-400">answers</span>
                 </h2>
-                <div className="mt-12 flex flex-col gap-6 max-w-xl mx-auto">
-                    {faqs.map((faq, faqIndex) => (
-                        <div
-                            key={faq.question}
-                            className="bg-neutral-900 rounded-2xl border border-white/10 p-6"
-                        >
-                            <div className="flex justify-between  items-center ">
-                                <h3 className="font-medium">{faq.question}</h3>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeWidth="2"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    className={twMerge(
-                                        "text-lime-400 flex-shrink-0 ",
-                                        selectedIndex === faqIndex &&
-                                            "rotate-45"
-                                    )}
-                                >
-                                    <line x1="12" y1="5" x2="12" y2="19"></line>
-                                    <line x1="5" y1="12" x2="19" y2="12"></line>
-                                </svg>
-                            </div>
+                <div className="relative flex flex-col items-center">
+                    <div className="mt-12 flex flex-col gap-6 max-w-xl mx-auto absolute">
+                        {faqs.map((faq, faqIndex) => (
                             <div
-                                className={twMerge(
-                                    "mt-6",
-                                    selectedIndex !== faqIndex && "hidden"
-                                )}
+                                key={faq.question}
+                                className="bg-neutral-900 rounded-2xl border border-white/10 p-6"
                             >
-                                <p className="text-white/50">{faq.answer}</p>
+                                <div
+                                    className="flex justify-between items-center"
+                                    onClick={() => {
+                                        setSelectedIndex(faqIndex);
+                                    }}
+                                >
+                                    <h3 className="font-medium">
+                                        {faq.question}
+                                    </h3>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="24"
+                                        height="24"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        strokeWidth="2"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        className={twMerge(
+                                            "text-lime-400 flex-shrink-0 transition duration-300",
+                                            selectedIndex === faqIndex &&
+                                                "rotate-45"
+                                        )}
+                                    >
+                                        <line
+                                            x1="12"
+                                            y1="5"
+                                            x2="12"
+                                            y2="19"
+                                        ></line>
+                                        <line
+                                            x1="5"
+                                            y1="12"
+                                            x2="19"
+                                            y2="12"
+                                        ></line>
+                                    </svg>
+                                </div>
+                                <AnimatePresence>
+                                    {" "}
+                                    {selectedIndex === faqIndex && (
+                                        <motion.div
+                                            initial={{
+                                                height: 0,
+                                                marginTop: 0,
+                                            }}
+                                            animate={{
+                                                height: "auto",
+                                                marginTop: 24,
+                                            }}
+                                            exit={{ height: 0, marginTop: 0 }}
+                                            className="overflow-hidden"
+                                        >
+                                            <p className="text-white/50">
+                                                {faq.answer}
+                                            </p>
+                                        </motion.div>
+                                    )}
+                                </AnimatePresence>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
+                    <div className="h-[576px]"></div>
                 </div>
             </div>
         </section>
