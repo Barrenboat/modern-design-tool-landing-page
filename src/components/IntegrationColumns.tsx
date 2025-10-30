5aaa4135a2f1c9ecbc80309b33ab1a29962b5f05
Barrenboat <lyz012@qq.com>
2025-10-30 09:54:09 +0800

    feat: 为IntegrationColumns组件添加了动画效果

--- src/components/IntegrationColumns.tsx
+++ src/components/IntegrationColumns.tsx
@@ -1,36 +1,54 @@
+"use client";
+
 import Image from "next/image";
 import type { IntegrationsType } from "@/sections/Integrations";
 import { twMerge } from "tailwind-merge";
+import { motion } from "framer-motion";
+import { Fragment } from "react";
 
 const IntegrationColumns = (props: {
     integrations: IntegrationsType;
     className?: string;
+    reverse?: boolean;
 }) => {
-    const { integrations, className } = props;
+    const { integrations, className, reverse } = props;
     return (
-        <div className={twMerge("flex flex-col gap-4 pb-4", className)}>
-            {integrations.map((integration) => (
-                <div
-                    key={integration.name}
-                    className="bg-neutral-900 border border-white/10 rounded-3xl p-6"
-                >
-                    <div className="flex justify-center">
-                        <Image
-                            src={integration.icon}
-                            alt={integration.name}
-                            className="size-24"
-                        />
-                    </div>
+        <motion.div
+            initial={{ y: reverse ? "-50%" : 0 }}
+            animate={{ y: reverse ? 0 : "-50%" }}
+            transition={{
+                duration: 30,
+                ease: "linear",
+                repeat: Infinity,
+            }}
+            className={twMerge("flex flex-col gap-4 pb-4", className)}
+        >
+            {Array.from({ length: 2 }, (_, i) => (
+                <Fragment key={i}>
+                    {integrations.map((integration) => (
+                        <div
+                            key={integration.name}
+                            className="bg-neutral-900 border border-white/10 rounded-3xl p-6"
+                        >
+                            <div className="flex justify-center">
+                                <Image
+                                    src={integration.icon}
+                                    alt={integration.name}
+                                    className="size-24"
+                                />
+                            </div>
 
-                    <h3 className="text-3xl text-center mt-6">
-                        {integration.name}
-                    </h3>
-                    <p className="text-center text-white/50 mt-2">
-                        {integration.description}
-                    </p>
-                </div>
+                            <h3 className="text-3xl text-center mt-6">
+                                {integration.name}
+                            </h3>
+                            <p className="text-center text-white/50 mt-2">
+                                {integration.description}
+                            </p>
+                        </div>
+                    ))}
+                </Fragment>
             ))}
-        </div>
+        </motion.div>
     );
 };
 
