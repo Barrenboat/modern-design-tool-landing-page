fc640999bf6b240527db8a6a5a403bf8f636b1e9
Barrenboat <lyz012@qq.com>
2025-10-30 17:28:51 +0800

    feat: CTA section滚动动画添加了hover减速和变色效果

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,26 +1,43 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { AnimationPlaybackControls, motion, useAnimate } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 
 export default function CallToAction() {
+    const animation = useRef<AnimationPlaybackControls>();
+    const [scope, animate] = useAnimate();
+    const [isHovered, setIsHovered] = useState(false);
+
+    useEffect(() => {
+        animation.current = animate(
+            scope.current,
+            { x: "-50%" },
+            { duration: 30, ease: "linear", repeat: Infinity }
+        );
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    useEffect(() => {
+        if (animation.current) animation.current.speed = isHovered ? 0.5 : 1;
+    }, [isHovered]);
+
     return (
         <section className="py-24">
             <div className="overflow-x-clip py-4 px-2 flex">
                 <motion.div
-                    animate={{ x: "-50%" }}
-                    transition={{
-                        duration: 30,
-                        ease: "linear",
-                        repeat: Infinity,
-                    }}
-                    className="flex flex-none gap-16 pr-16 text-7xl md:text-8xl font-medium"
+                    ref={scope}
+                    className="flex flex-none gap-16 pr-16 text-7xl md:text-8xl font-medium group cursor-pointer"
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
                 >
                     {Array.from({ length: 10 }, (_, index) => (
                         <div key={index} className="flex items-center gap-14">
                             <span className="text-lime-400 text-7xl">
                                 &#10038;
                             </span>
-                            <span>Try it for free</span>
+                            <span className="group-hover:text-lime-400">
+                                Try it for free
+                            </span>
                         </div>
                     ))}
                 </motion.div>
