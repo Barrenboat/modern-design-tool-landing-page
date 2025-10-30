7b7d29e2c578786a1d049e4d2bf2ee0a57d133ba
Barrenboat <lyz012@qq.com>
2025-10-30 09:13:56 +0800

    feat: 为组件添加group-hover触发的按下效果，需父组件添加group样式

--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -5,7 +5,7 @@ const Key = (props: HTMLAttributes<HTMLDivElement>) => {
     return (
         <div
             className={twMerge(
-                "size-14 bg-neutral-300 inline-flex items-center justify-center rounded-2xl text-xl text-neutral-950 font-medium",
+                "size-14 bg-neutral-300 inline-flex items-center justify-center rounded-2xl text-xl text-neutral-950 font-medium outline outline-2 outline-offset-4 outline-transparent group-hover:outline-lime-400 group-hover:translate-y-1 transition-all duration-500 will-change-transform",
                 className
             )}
             {...otherProps}
